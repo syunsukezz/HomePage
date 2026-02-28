0fef8b78f136b35be0e23aa99865436fdfa96c0e
syunsukezz <139102798+syunsukezz@users.noreply.github.com>
2026-02-28 19:57:03 +0900

    vite.configを追加

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+// import viteCompression from 'vite-plugin-compression'
+
+export default defineConfig({
+  plugins: [
+    // コメントアウトまたは削除: viteCompression({ algorithm: 'brotliCompress', ext: '.br' })
+  ],
+})
